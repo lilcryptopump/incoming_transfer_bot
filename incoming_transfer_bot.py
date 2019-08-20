e95f7efe3bc0835cf9e91746bb45b7ca58ae337f
Brendan Jensen <brendanjensenrecordings@gmail.com>
2019-08-19 21:47:45 -0500

    Update incoming_transfer_bot.py

--- incoming_transfer_bot.py
+++ incoming_transfer_bot.py
@@ -10,9 +10,9 @@ ACCOUNT_WATCHING = 'iamredbar1'
 BOT_PHONE_NUMBER = '+17637031688'
 YOUR_PHONE_NUMBER = '+19522128926'
 
-#account_sid = 'AC0d3a91069b2604617ad1e765414f6631'
-#auth_token = ''
-#client = Client(account_sid, auth_token)
+account_sid = 'AC0d3a91069b2604617ad1e765414f6631'
+auth_token = ''
+client = Client(account_sid, auth_token)
 
 
 bitshares = BitShares(
@@ -31,23 +31,26 @@ for op in blockchain.stream(['transfer']):
     payee = Account(op['to']).name
     from_account = Account(op['from']).name
     asset_symbol = Asset(op['amount']['asset_id']).symbol
-    asset_precision = Asset(op['amount']['asset_id']).precision
-    amount = op['amount']['amount'] / (10*asset_precision)
+    asset_precision = int(Asset(op['amount']['asset_id']).precision)
+    amount = int(op['amount']['amount'])
+    if asset_precision not 0:
+        amount = amount / (10*asset_precision)
     pprint('{} sent {} {} {} in block {}.'.format(
                                     from_account,
                                     payee,
                                     amount,
                                     asset_symbol,
                                     op['block_num']))
-#    if payee == ACCOUNT_WATCHING:
-#        message = client.messages.create(
-#                                body='{} sent you {} {} in block {}.'.format(
-#                                    op['from'],
-#                                    op['amount'],
-#                                    op['amount'],
-#                                    op['block_num']
-#                               ),
-#                                from_=BOT_PHONE_NUMBER,
-#                               to=YOUR_PHONE_NUMBER
-#                                )
-#        print(message.sid)
+    if payee == ACCOUNT_WATCHING:
+        message = client.messages.create(
+                                body='{} sent {} {} {} in block {}.'.format(
+                                    from_account,
+                                    payee,
+                                    amount,
+                                    asset_symbol,
+                                    op['block_num']
+                               ),
+                                from_=BOT_PHONE_NUMBER,
+                               to=YOUR_PHONE_NUMBER
+                                )
+        print(message.sid)
